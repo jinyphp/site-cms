c5cc2050ac47f0b19433e3e10caf425f9a0c28ad
hojinlee <infohojin@gmail.com>
2024-07-24 14:29:10 +0900

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# SiteCMS
+Site Content Management 패키지 입니다. `jiny/site`를 기반으로 웹사이트의 컨덴츠를 관리할 수 있습니다.
+
+## 설치
+본 패키지는 `laravel` 과 `jinyphp`의 기능을 확장하기 위한 페키지 입니다. 또한, `jiny/site`페키지에 의존되어 있습니다.
+
+```
+composer require jiny/site-cms
+```
---
+++ src/Helpers/JinySiteCMSServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+namespace Jiny\Site\CMS;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\Support\Facades\File;
+use Livewire\Livewire;
+
+class JinySiteCMSServiceProvider extends ServiceProvider
+{
+    private $package = "jiny-site-cms";
+    public function boot()
+    {
+        // 모듈: 라우트 설정
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', $this->package);
+
+        // 데이터베이스
+        $this->loadMigrationsFrom(__DIR__.'/../databases/migrations');
+
+    }
+
+    public function register()
+    {
+        /* 라이브와이어 컴포넌트 등록 */
+        $this->app->afterResolving(BladeCompiler::class, function () {
+            
+
+        });
+    }
+}
